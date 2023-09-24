79cbb9e66d9cf0b46d8dacc707cc9d3b4f2f6eb4
irelius <kihong711@hotmail.com>
2023-09-23 20:43:40 -0700

    resettin database

---
+++ backend/db/seeders/20230924034259-user-seeder.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const { User } = require('../models');
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    options.tableName = "Users";
+    return queryInterface.bulkInsert(options, [
+      {
+        email: 'demo@aa.io',
+        firstName: "Demo",
+        lastName: "User",
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'user1@aa.io',
+        firstName: "User",
+        lastName: "One",
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'user2@aa.io',
+        firstName: "User",
+        lastName: "Two",
+        hashedPassword: bcrypt.hashSync('password')
+      }
+    ], { validate: true });
+  },
+
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'Users';
+    return queryInterface.bulkDelete(options, {}, {});
+  }
+};
