b1c9091ee301700c90164f6063ebe1c6756c0185
irelius <kihong711@hotmail.com>
2024-04-17 21:54:26 -0700

    progress paused to work on implementing sass for increased efficiency in styling

--- frontend/src/components/SpotPageImage/SpotPageImage.js
+++ frontend/src/components/SpotPageImage/SpotPageImage.js
@@ -1,9 +1,59 @@
 import "./SpotPageImage.css"
 
 const SpotPageImage = ({ images }) => {
-    return (
+    console.log('booba', images)
+
+    const testLength = [1, 2]
+
+    return testLength.length === 1 ? (
+        <div className="spot-page-image-primary">
+            <img src={`${images[0].url}`} alt='test' />
+        </div>
+    ) : testLength.length === 2 ? (
         <div>
-            booba
+            <aside className="spot-page-image-primary">
+                <img src={`${images[0].url}`} alt='test' />
+            </aside>
+            <aside className="spot-page-image-primary">
+                <img src={`${images[0].url}`} alt='test' />
+            </aside>
+        </div>
+    ) : testLength.length === 3 ? (
+        <div>
+            <section className="spot-page-image-primary">
+                <img src={`${images[0].url}`} alt='test' />
+            </section>
+            <section>
+                <aside><img src={`${images[0].url}`} alt='test' /></aside>
+                <aside><img src={`${images[0].url}`} alt='test' /></aside>
+            </section>
+        </div>
+    ) : testLength.length === 4 ? (
+        <div>
+            <section className="spot-page-image-primary"><img src={`${images[0].url}`} alt='test' /></section>
+            <section>
+                <section><img src={`${images[0].url}`} alt='test' /></section>
+                <section>
+                    <aside><img src={`${images[0].url}`} alt='test' /></aside>
+                    <aside><img src={`${images[0].url}`} alt='test' /></aside>
+                </section>
+            </section>
+        </div>
+    ) : (
+        <div>
+            <section className="spot-page-image-primary">
+                <img src={`${images[0].url}`} alt='test' />
+            </section>
+            <section>
+                <section>
+                    <aside><img src={`${images[0].url}`} alt='test' /></aside>
+                    <aside><img src={`${images[0].url}`} alt='test' /></aside>
+                </section>
+                <section>
+                    <aside><img src={`${images[0].url}`} alt='test' /></aside>
+                    <aside><img src={`${images[0].url}`} alt='test' /></aside>
+                </section>
+            </section>
         </div>
     )
 }
