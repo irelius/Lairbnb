105244850289bd380f134093a24d98bfc81533e4
irelius <kihong711@hotmail.com>
2024-04-23 02:40:20 -0700

    added new link to portolio site on footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -7,8 +7,9 @@ function Footer() {
                 <p>Samuel Bae - 2024 Lairbnb</p>
             </div>
             <div className="footer-right">
-                <a href="https://www.linkedin.com/in/sbkihongbae/">LinkedIn</a>
-                <a href="https://github.com/irelius">GitHub</a>
+                <a href="https://samuelbae.netlify.app" target="_blank" rel="noreferrer">Portfolio</a>
+                <a href="https://www.linkedin.com/in/sbkihongbae/" target="_blank" rel="noreferrer">LinkedIn</a>
+                <a href="https://github.com/irelius" target="_blank" rel="noreferrer">GitHub</a>
             </div>
         </div>
     )
