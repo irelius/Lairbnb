2be8dc4fb8389baaec3091a34cf36296ccd22aa9
irelius <kihong711@hotmail.com>
2024-05-09 20:41:34 -0700

    refactored review modal to be styled and changed rating input from a number input and interactable star rating

--- frontend/src/components/Modals/ReviewModal/ReviewForm.js
+++ frontend/src/components/Modals/ReviewModal/ReviewForm.js
@@ -1,3 +1,5 @@
+import "./ReviewForm.css"
+
 import { useParams } from "react-router-dom/cjs/react-router-dom.min"
 import { useDispatch } from "react-redux"
 import { useState } from "react"
@@ -12,7 +14,7 @@ function ReviewForm() {
     const spotId = useParams().spotId;
 
     const [review, setReview] = useState("")
-    const [stars, setStars] = useState(0)
+    const [stars, setStars] = useState(null)
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -27,30 +29,36 @@ function ReviewForm() {
     }
 
     return (
-        <div className="modal-form-container ffffff-bg" onClick={(e) => e.stopPropagation()}>
-            <div className="submit-review-main">
+        <div className="review-form-container" onClick={(e) => e.stopPropagation()}>
+            <section className="review-exit-button mouse-pointer bg-off-white-hover" onClick={() => { }}>
+                <i className="fa-solid fa-xmark" />
+            </section>
+            <div className="review-form-section">
                 <form onSubmit={(e) => {
                     e.stopPropagation()
                     handleSubmit(e)
                 }} className="review-form">
-                    <p className="review-title">Type Your Review</p>
-                    <input
+                    <section className="review-form-title">Type Your Review</section>
+
+                    <textarea
+                        className="review-input border-black"
                         type="text"
                         required
                         value={review}
                         onChange={(e) => setReview(e.target.value)}
                         onClick={(e) => e.stopPropagation()}
                     />
-                    <p className="review-title">Give This Location a Rating</p>
-                    <input
-                        type="number"
-                        required
-                        min="1"
-                        max="5"
-                        value={stars}
-                        onChange={(e) => setStars(e.target.value)}
-                        onClick={(e) => e.stopPropagation()}
-                    />
+
+                    <section className="">Give This Location a Rating</section>
+
+                    <section className="df-r">
+                        <i className={`star-${stars >= 1 ? "selected": "unselected"}  mouse-pointer fa-solid fa-star fa-xl`} onClick={() => {setStars(1)}}></i>
+                        <i className={`star-${stars >= 2 ? "selected": "unselected"}  mouse-pointer fa-solid fa-star fa-xl`} onClick={() => {setStars(2)}}></i>
+                        <i className={`star-${stars >= 3 ? "selected": "unselected"}  mouse-pointer fa-solid fa-star fa-xl`} onClick={() => {setStars(3)}}></i>
+                        <i className={`star-${stars >= 4 ? "selected": "unselected"}  mouse-pointer fa-solid fa-star fa-xl`} onClick={() => {setStars(4)}}></i>
+                        <i className={`star-${stars >= 5 ? "selected": "unselected"}  mouse-pointer fa-solid fa-star fa-xl`} onClick={() => {setStars(5)}}></i>
+                    </section>
+
                     <button
                         type="submit"
                         className="submit-review-button bg-white border-none"
