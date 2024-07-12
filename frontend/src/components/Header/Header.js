1aa1a7fbc6464d110d6db1ba3aca02ff04506824
irelius <kihong711@hotmail.com>
2024-07-12 16:40:47 -0700

    adjust header pading

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -12,35 +12,37 @@ function Header({ isLoaded }) {
     const user = useSelector(state => state.user);
 
     return (
-        <div className="header-main-container border-bot-235">
-            <aside>
-                <section className="header-lairbnb-icon-container mouse-pointer" onClick={() => history.push("/")}>
-                    <img className="lairbnb-icon" src="https://raw.githubusercontent.com/irelius/Airbnb/main/frontend/public/assets/favicon-32x32.png"
-                        alt="lairbnb-icon"
-                    />
-                    <p className="lairbnb-header-text font-bold">
-                        Lairbnb
-                    </p>
-                </section>
-            </aside>
-            {/* If user is logged in */}
-            {isLoaded && user.loggedIn === true ? (
-                <div className="header-right-container">
-                    <aside className="header-host-button-container">
-                        <button className="header-host-button bg-off-white-hover bold mouse-pointer" onClick={() => history.push("/become-a-host/property-form")}>
-                            Become a host
-                        </button>
-                    </aside>
-                    <ProfileDropdownMenu />
-                </div>
-                // {/* If user is not logged in */}
-            ) : isLoaded && user.loggedIn === false ? (
-                <div className="header-right-container">
-                    <ProfileDropdownMenu />
-                </div>
-            ) : (
-                <></>
-            )}
+        <div className='header-wrapper'>
+            <div className="header-main-container border-bot-235">
+                <aside>
+                    <section className="header-lairbnb-icon-container mouse-pointer" onClick={() => history.push("/")}>
+                        <img className="lairbnb-icon" src="https://raw.githubusercontent.com/irelius/Airbnb/main/frontend/public/assets/favicon-32x32.png"
+                            alt="lairbnb-icon"
+                        />
+                        <p className="lairbnb-header-text font-bold">
+                            Lairbnb
+                        </p>
+                    </section>
+                </aside>
+                {/* If user is logged in */}
+                {isLoaded && user.loggedIn === true ? (
+                    <div className="header-right-container">
+                        <aside className="header-host-button-container">
+                            <button className="header-host-button bg-off-white-hover bold mouse-pointer" onClick={() => history.push("/become-a-host/property-form")}>
+                                Become a host
+                            </button>
+                        </aside>
+                        <ProfileDropdownMenu />
+                    </div>
+                    // {/* If user is not logged in */}
+                ) : isLoaded && user.loggedIn === false ? (
+                    <div className="header-right-container">
+                        <ProfileDropdownMenu />
+                    </div>
+                ) : (
+                    <></>
+                )}
+            </div>
         </div>
     )
 }
