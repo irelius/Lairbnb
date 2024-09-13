ea3e582a07c38ac72cc563358cedbc73e0eb9fb3
irelius <kihong711@hotmail.com>
2024-09-12 22:22:27 -0700

    testing changing environment for seeding and migrating

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,28 +1,28 @@
-// backend/config/database.js
+// // backend/config/database.js
 // const config = require('./index');
 
 // module.exports = {
-//   development: {
-//     storage: config.dbFile,
-//     dialect: "sqlite",
-//     seederStorage: "sequelize",
-//     logQueryParameters: true,
-//     typeValidation: true
-//   },
-//   production: {
-//     use_env_variable: 'DATABASE_URL',
-//     dialect: 'postgres',
-//     seederStorage: 'sequelize',
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false
+//       development: {
+//             storage: config.dbFile,
+//             dialect: "sqlite",
+//             seederStorage: "sequelize",
+//             logQueryParameters: true,
+//             typeValidation: true
+//       },
+//       production: {
+//             use_env_variable: 'DATABASE_URL',
+//             dialect: 'postgres',
+//             seederStorage: 'sequelize',
+//             dialectOptions: {
+//                   ssl: {
+//                         require: true,
+//                         rejectUnauthorized: false
+//                   }
+//             },
+//             define: {
+//                   schema: process.env.SCHEMA
+//             }
 //       }
-//     },
-//     define: {
-//       schema: process.env.SCHEMA
-//     }
-//   }
 // };
 
 // backend/config/database.js
