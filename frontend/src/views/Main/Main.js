fb3707d1b2b6cc82449348b075d8decd087ba864
irelius <kihong711@hotmail.com>
2025-11-17 17:52:08 -0800

    fix header css

--- frontend/src/views/Main/Main.js
+++ frontend/src/views/Main/Main.js
@@ -1,35 +1,35 @@
-import "./Main.css"
-import React, { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
+import "./Main.css";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Spot from "../../components/Spot/Spot";
 import { loadAllSpotsThunk } from "../../store/spot";
 import { loadAllImagesThunk } from "../../store/image";
 
 function Main() {
-    const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-    const [load, setLoad] = useState(false)
+	const [load, setLoad] = useState(false);
 
-    useEffect(() => {
-        dispatch(loadAllSpotsThunk())
-        // dispatch(loadAllImagesThunk())
-        setLoad(true)
-    }, [dispatch])
+	useEffect(() => {
+		dispatch(loadAllSpotsThunk());
+		// dispatch(loadAllImagesThunk())
+		setLoad(true);
+	}, [dispatch]);
 
-    const spots = useSelector(state => state.spot)
-    // const images = useSelector(state => state.image)
+	const spots = useSelector((state) => state.spot);
+	// const images = useSelector(state => state.image)
 
-    return load ? (
-        <div className="all-spots">
-            {spots.spotIds.map((id, i) => {
-                return (
-                    <Spot spot={spots.spots[id]} key={i} />
-                )
-            })}
-        </div>
-    ) : (
-        <></>
-    )
+	return load ? (
+		<div className="all-spots-container">
+			<div className="all-spots">
+				{spots.spotIds.map((id, i) => {
+					return <Spot spot={spots.spots[id]} key={i} />;
+				})}
+			</div>
+		</div>
+	) : (
+		<></>
+	);
 }
 
-export default Main
+export default Main;
